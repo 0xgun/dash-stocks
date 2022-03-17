650497d3f3b4e8804cf2021189d47913ec0d5f61
Ganeshan <ganeshanmadesh36@outlook.com>
2022-03-17 21:43:17 +0530

    web layout

--- app.py
+++ app.py
@@ -1,7 +1,53 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from datetime import datetime as dt
 
 
 app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+    html.Div(
+          [
+            html.P("Welcome to the Stock Dash App!", className="start"),
+            html.Div([
+              # stock code input
+            ]),
+            html.Div([
+              # Date range picker input
+            ]),
+            html.Div([
+              # Stock price button
+              # Indicators button
+              # Number of days of forecast input
+              # Forecast button
+            ]),
+          ],
+        className="nav"),
+
+html.Div(
+          [
+            html.Div(
+                  [  # Logo
+                    # Company Name
+                  ],
+                className="header"),
+            html.Div( #Description
+              id="description", className="decription_ticker"),
+            html.Div([
+                # Stock price plot
+            ], id="graphs-content"),
+            html.Div([
+                # Indicator plot
+            ], id="main-content"),
+            html.Div([
+                # Forecast plot
+            ], id="forecast-content")
+          ],
+        className="content")])
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
