98dc013b1477542346e234b598a80f4f5fb84d0f
Ganeshan <ganeshanmadesh36@outlook.com>
2022-03-17 23:21:02 +0530

    Layout

--- app.py
+++ app.py
@@ -1,53 +1,87 @@
 import dash
 from dash import dcc
 from dash import html
-from datetime import datetime as dt
+from datetime import date
 
 
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div([
-    html.Div(
-          [
-            html.P("Welcome to the Stock Dash App!", className="start"),
-            html.Div([
-              # stock code input
-            ]),
-            html.Div([
-              # Date range picker input
-            ]),
-            html.Div([
-              # Stock price button
-              # Indicators button
-              # Number of days of forecast input
-              # Forecast button
-            ]),
-          ],
-        className="nav"),
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                html.P("Welcome to the Stock Dash App!", className="start"),
+                html.Div(
+                    [
+                        # stock code input
+                        html.P("Input the stock code:"),
+                        dcc.Input(
+                            id="input-code",
+                        ),
+                       html.Button("Submit"),
+                        # Date range picker input
+                        html.Div(
+                            [
+                                dcc.DatePickerRange(
+                                    id="date-picker-range",
+                                    start_date_placeholder_text="Start date",
+                                    end_date=date(2022, 3, 16),
+                                )
+                            ]
+                        ),
+                    ]
+                ),
+                html.Div(
+                    [
+                        # Stock price button
+                        html.Button("Stock Price"),
+                        # Indicators button
+                        html.Button("Indicators"),
+                        # Number of days of forecast input
+                        dcc.Input(id="input-box", type="text"),
+                        # Forecast button
+                        html.Button("Forecast"),
+                    ]
+                ),
+            ],
+            className="nav",
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [  # Logo
+                        # Company Name
+                    ],
+                    className="header",
+                ),
+                html.Div(  # Description
+                    id="description", className="decription_ticker"
+                ),
+                html.Div(
+                    [
+                        # Stock price plot
+                    ],
+                    id="graphs-content",
+                ),
+                html.Div(
+                    [
+                        # Indicator plot
+                    ],
+                    id="main-content",
+                ),
+                html.Div(
+                    [
+                        # Forecast plot
+                    ],
+                    id="forecast-content",
+                ),
+            ],
+            className="content",
+        ),
+    ]
+)
 
-html.Div(
-          [
-            html.Div(
-                  [  # Logo
-                    # Company Name
-                  ],
-                className="header"),
-            html.Div( #Description
-              id="description", className="decription_ticker"),
-            html.Div([
-                # Stock price plot
-            ], id="graphs-content"),
-            html.Div([
-                # Indicator plot
-            ], id="main-content"),
-            html.Div([
-                # Forecast plot
-            ], id="forecast-content")
-          ],
-        className="content")])
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
